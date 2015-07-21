33d3f606db9efa699ae4859094292e94fc06186c
Jason Frame <jason@onehackoranother.com>
2015-07-21 20:41:55 +0100

    tabs -> spaces

--- index.js
+++ index.js
@@ -8,41 +8,41 @@ var DEFAULT_DELAY = 30000;
 module.exports = attractor;
 
 function attractor(opts) {
-	var state 			= 'off',
-		events 			= new EventBox(),
-		activate 		= opts.activate,
-		deactivate 		= opts.deactivate,
-		bindUiEvents 	= opts.events || DEFAULT_EVENTS,
-		timer 			= timeout(_activate, opts.delay || DEFAULT_DELAY);
-	
-	bindUiEvents.forEach(function(e) {
-		document.body.addEventListener(e, _deactivate, true);	
-	});
+    var state           = 'off',
+        events          = new EventBox(),
+        activate        = opts.activate,
+        deactivate      = opts.deactivate,
+        bindUiEvents    = opts.events || DEFAULT_EVENTS,
+        timer           = timeout(_activate, opts.delay || DEFAULT_DELAY);
+    
+    bindUiEvents.forEach(function(e) {
+        document.body.addEventListener(e, _deactivate, true);   
+    });
 
-	function _activate(firstTime) {
-		if (state !== 'off') return;
-		state = 'off->on';
-		events.emit('willActivate');
-		activate(!!firstTime).then(function() {
-			state = 'on';
-			events.emit('didActivate');
-		});
-	}
+    function _activate(firstTime) {
+        if (state !== 'off') return;
+        state = 'off->on';
+        events.emit('willActivate');
+        activate(!!firstTime).then(function() {
+            state = 'on';
+            events.emit('didActivate');
+        });
+    }
 
-	function _deactivate() {
-		if (state !== 'on') return;
-		state = 'on->off';
-		events.emit('willDeactivate');
-		deactivate().then(function() {
-			state = 'off';
-			events.emit('didDeactivate');
-			timer.start();
-		});
-	}
+    function _deactivate() {
+        if (state !== 'on') return;
+        state = 'on->off';
+        events.emit('willDeactivate');
+        deactivate().then(function() {
+            state = 'off';
+            events.emit('didDeactivate');
+            timer.start();
+        });
+    }
 
-	events.start = function() {
-		_activate(true);
-	}
+    events.start = function() {
+        _activate(true);
+    }
 
-	return events;
+    return events;
 }
